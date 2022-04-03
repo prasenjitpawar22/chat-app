8fa9d1f4f7a5fe923c4e3e8ec61eda52d60e4d7c
prasenjitpawar22 <prasenjitpawar22@gmail.com>
2022-04-03 14:37:27 +0530

    first

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express = require("express");
+import { createServer } from "http";
+import { Server } from "socket.io";
+import {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "./types";
+
+const app = express();
+const server = createServer(app);
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("sendMsg", (payload) => {
+    console.log(payload);
+    io.emit("chat", payload);
+  });
+  // console.log("what is socket", socket.id);
+});
+
+server.listen(3100, () => console.log("lsting on port 3100"));
---
+++ src/types/types.ts
@@ -0,0 +1,20 @@
+export interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+
+  chat: (a: string) => void;
+}
+
+export interface ClientToServerEvents {
+  sendMsg: (arg1: string) => void;
+}
+
+export interface InterServerEvents {
+  ping: () => void;
+}
+
+export interface SocketData {
+  name: string;
+  age: number;
+}
